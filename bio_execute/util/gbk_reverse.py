acb97de83789b2fc043eba5bb01c54817d78b9d8
bombumbom <99683007+bombumbom@users.noreply.github.com>
2023-07-07 13:56:32 +0900

    Update gbk_reverse.py

--- bio_execute/util/gbk_reverse.py
+++ bio_execute/util/gbk_reverse.py
@@ -17,16 +17,7 @@ filename = filename.split('.gb')
 filename = filename[0]
 
 record = SeqIO.read(file, "genbank")
-#print("%s %i %i %i %i" % (record.id, len(record), len(record.features),len(record.dbxrefs), len(record.annotations)))
 
 rc = record.reverse_complement(id="rev_"+record.id, description = "reverse complement", annotations=record.annotations)
-#
-#print("%s %i %i %i %i" % (rc.id, len(rc), len(rc.features), len(rc.dbxrefs),len(rc.annotations)))
-#
-# for feature in rc.features:
-#     if feature.type == 'CDS':
-#         print("CDS  == " + feature.qualifiers['gene'][0])
-#         print(str(feature.location.extract))
 
-#print(filename.split('.gb'))
 SeqIO.write(rc,args.p+filename+'_rev.gb',"genbank")
